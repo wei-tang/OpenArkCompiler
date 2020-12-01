d0ea48fefdd34ca547e9a8fe007db7418c4f62b8
binaryfz <fuzhou@huawei.com>
2020-12-01 10:36:58 +0800

    update mapleall lcn header,add dex2mpl

--- src/mapleall/maple_util/include/file_layout.h
+++ src/mapleall/maple_util/include/file_layout.h
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) [2019-2020] Huawei Technologies Co.,Ltd.All rights reserved.
  *
- * OpenArkCompiler is licensed under the Mulan PSL v1.
- * You can use this software according to the terms and conditions of the Mulan PSL v1.
- * You may obtain a copy of Mulan PSL v1 at:
+ * OpenArkCompiler is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
  *
- *     http://license.coscl.org.cn/MulanPSL
+ *     http://license.coscl.org.cn/MulanPSL2
  *
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
  * FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PSL v1 for more details.
+ * See the Mulan PSL v2 for more details.
  */
-#ifndef MAPLE_UTIL_INCLUDE_FILE_LAYOUT_H
-#define MAPLE_UTIL_INCLUDE_FILE_LAYOUT_H
+#ifndef MIRLAYOUT_H
+#define MIRLAYOUT_H
 #include <iostream>
 
 namespace maple {
