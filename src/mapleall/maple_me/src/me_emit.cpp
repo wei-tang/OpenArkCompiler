efdcecf288328bc74aa9835cd42cfc92f4fca309
linma <lma1@futurewei.com>
2021-05-04 18:31:35 -0700

    check function IR status (hssa or not) according to phase name in me_emit

--- src/mapleall/maple_me/src/me_emit.cpp
+++ src/mapleall/maple_me/src/me_emit.cpp
@@ -20,34 +20,54 @@
 #include "me_cfg.h"
 #include "constantfold.h"
 
+static const std::set<std::string> kHssaPhases{
+  "irmapbuild",  "hprop",      "hssa",        "hdse",        "ssadevirt",    "ea",          "epre",
+  "lpre",   "stmtpre",    "rename2preg", "ti",          "analyzerc",
+  "spre",   "rclowering", "delegaterc",  "condbasedrc", "condbasednpc", "may2dassign", "pregrename",
+  "bdcopt", "syncselect", "cfgopt",      "placementrc",  "symrename",   "checkcastopt", "bblayout",
+};
+
 namespace maple {
 // emit IR to specified file
-AnalysisResult *MeDoEmit::Run(MeFunction *func, MeFuncResultMgr*, ModuleResultMgr*) {
+AnalysisResult *MeDoEmit::Run(MeFunction *func, MeFuncResultMgr *m, ModuleResultMgr*) {
   static std::mutex mtx;
   ParallelGuard guard(mtx, ThreadEnv::IsMeParallel());
   if (func->NumBBs() > 0) {
-    CHECK_FATAL(func->GetIRMap() != nullptr, "Why not hssa?");
-    // generate bblist after layout (bb physical position)
-    CHECK_FATAL(func->HasLaidOut(), "Check/Run bb layout phase.");
-    auto layoutBBs = func->GetLaidOutBBs();
-    MIRFunction *mirFunction = func->GetMirFunc();
-    mirFunction->ReleaseCodeMemory();
+    std::string passName = GetPreviousPhaseName();
+    bool emitHssaOrAfter = kHssaPhases.find(std::string(passName)) != kHssaPhases.end();
+    if (emitHssaOrAfter) {
+      CHECK_FATAL(func->GetIRMap() != nullptr, "Why not hssa?");
+      // generate bblist after layout (bb physical position)
+      CHECK_FATAL(func->HasLaidOut(), "Check/Run bb layout phase.");
+      auto layoutBBs = func->GetLaidOutBBs();
+      MIRFunction *mirFunction = func->GetMirFunc();
+      mirFunction->ReleaseCodeMemory();
 
-    mirFunction->SetMemPool(new ThreadLocalMemPool(memPoolCtrler, "IR from IRMap::Emit()"));
-    mirFunction->SetBody(mirFunction->GetCodeMempool()->New<BlockNode>());
-    // initialize is_deleted field to true; will reset when emitting Maple IR
-    for (size_t k = 1; k < mirFunction->GetSymTab()->GetSymbolTableSize(); ++k) {
-      MIRSymbol *sym = mirFunction->GetSymTab()->GetSymbolFromStIdx(k);
-      if (sym->GetSKind() == kStVar) {
-        sym->SetIsDeleted();
+      mirFunction->SetMemPool(new ThreadLocalMemPool(memPoolCtrler, "IR from IRMap::Emit()"));
+      mirFunction->SetBody(mirFunction->GetCodeMempool()->New<BlockNode>());
+      // initialize is_deleted field to true; will reset when emitting Maple IR
+      for (size_t k = 1; k < mirFunction->GetSymTab()->GetSymbolTableSize(); ++k) {
+        MIRSymbol *sym = mirFunction->GetSymTab()->GetSymbolFromStIdx(k);
+        if (sym->GetSKind() == kStVar) {
+          sym->SetIsDeleted();
+        }
       }
+      for (BB *bb : layoutBBs) {
+        ASSERT(bb != nullptr, "Check bblayout phase");
+        bb->EmitBB(*func->GetMeSSATab(), *mirFunction->GetBody(), false);
+      }
+    } else {
+      if(!func->HasLaidOut()) {
+        (void)m->GetAnalysisResult(MeFuncPhase_BBLAYOUT, func);
+        CHECK_FATAL(func->HasLaidOut(), "Check bb layout phase.");
+      }
+      func->EmitBeforeHSSA((*(func->GetMirFunc())), func->GetLaidOutBBs());
     }
-    for (BB *bb : layoutBBs) {
-      ASSERT(bb != nullptr, "Check bblayout phase");
-      bb->EmitBB(*func->GetMeSSATab(), *mirFunction->GetBody(), false);
+    if (!DEBUGFUNC(func)) {
+      // constantfolding does not update BB's stmtNodeList, which breaks MirCFG::DumpToFile()
+      ConstantFold cf(func->GetMIRModule());
+      cf.Simplify(func->GetMirFunc()->GetBody());
     }
-    ConstantFold cf(func->GetMIRModule());
-    cf.Simplify(func->GetMirFunc()->GetBody());
     if (DEBUGFUNC(func)) {
       LogInfo::MapleLogger() << "\n==============after meemit =============" << '\n';
       func->GetMirFunc()->Dump();
