3e327059b0c7c731f4ebcf70214a8ffda24196eb
Wen HU <git_huwen@yahoo.com>
2021-05-18 17:09:42 -0400

    use clang2mpl instead of whirl2mpl

--- testsuite/driver/src/api/clang2mpl.py
+++ testsuite/driver/src/api/clang2mpl.py
@@ -12,19 +12,15 @@
 # See the Mulan PSL v2 for more details.
 #
 
-import os
-
 from api.shell_operator import ShellOperator
 
 
-class Clangfe(ShellOperator):
-
-    aarch64_linux_gnu_version = os.listdir("/usr/lib/gcc-cross/aarch64-linux-gnu")[0]
+class Clang2mpl(ShellOperator):
 
     def __init__(self, infile, return_value_list=None, redirection=None):
         super().__init__(return_value_list, redirection)
         self.infile = infile
 
     def get_command(self, variables):
-        self.command = "${TOOL_BIN_PATH}/clangfe -cc1 -emit-llvm -triple aarch64-linux-gnu -D__clang__ -D__BLOCKS__ -isystem /usr/aarch64-linux-gnu/include -isystem /usr/lib/gcc-cross/aarch64-linux-gnu/" + str(Clangfe.aarch64_linux_gnu_version) + "/include " + self.infile
+        self.command = "${MAPLE_ROOT}/output/aarch64-clang-release/bin/clang2mpl --ascii " + self.infile + " -- --target=aarch64-linux-elf "
         return super().get_final_command(variables)
