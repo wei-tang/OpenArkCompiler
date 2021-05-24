bcb7a7f0d6a66603df0dfe4e7b7ac7c60b53fc4f
Brice Dobry <brice.dobry@futurewei.com>
2021-05-24 11:01:01 -0400

    `isystem` and `sysroot` flags are now hard-coded
    Users no longer need to know/remember to include these flags as they are
    now hard-coded into clang2mpl.

--- testsuite/driver/src/api/clang2mpl.py
+++ testsuite/driver/src/api/clang2mpl.py
@@ -22,7 +22,6 @@ class Clang2mpl(ShellOperator):
         self.infile = infile
 
     def get_command(self, variables):
-        isystemFlags = "-isystem ${MAPLE_ROOT}/tools/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04/lib/clang/10.0.0/include --sysroot=${MAPLE_ROOT}/tools/sysroot-glibc-linaro-2.25"
-        clangFlags = "--target=aarch64-linux-elf -Wno-return-type -U__SIZEOF_INT128__ " + isystemFlags
+        clangFlags = "--target=aarch64-linux-elf -Wno-return-type -U__SIZEOF_INT128__ "
         self.command = "${MAPLE_EXECUTE_BIN}/clang2mpl --ascii " + self.infile + " -- " + clangFlags
         return super().get_final_command(variables)
